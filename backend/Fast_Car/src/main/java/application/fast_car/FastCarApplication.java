a2dfb82ea3d4fb672c60081502abe24ccdc76799
Helio-fagundes <helio.fagundes.dev@gmail.com>
2025-06-05 17:15:22 -0300

    git init backend

---
+++ backend/Fast_Car/src/main/java/application/fast_car/FastCarApplication.java
@@ -0,0 +1,13 @@
+package application.fast_car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastCarApplication.class, args);
+    }
+
+}
---
+++ backend/Fast_Car/src/test/java/application/fast_car/FastCarApplicationTests.java
@@ -0,0 +1,13 @@
+package application.fast_car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
